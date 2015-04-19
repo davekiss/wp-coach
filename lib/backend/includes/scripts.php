f9adbad49262ffad5b1aa4b0d35143b0df93e2ae
Dave Kiss <hello@davekiss.com>
2015-04-19 18:31:48 -0400

    Add FA and styling for admin tables

--- lib/backend/includes/scripts.php
+++ lib/backend/includes/scripts.php
@@ -17,9 +17,11 @@ class WP_Coach_Admin_Scripts {
 
     wp_register_script('magnific-popup', WP_COACH_URL . 'lib/backend/assets/bower_components/magnific-popup/dist/jquery.magnific-popup.min.js', array('jquery') );
     wp_register_style('magnific-popup', WP_COACH_URL . 'lib/backend/assets/bower_components/magnific-popup/dist/magnific-popup.css');
+    wp_register_style('fontawesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css');
 
     wp_enqueue_script('magnific-popup');
     wp_enqueue_style('magnific-popup');
+    wp_enqueue_style('fontawesome');
 
     wp_register_script('knockout', '//cdnjs.cloudflare.com/ajax/libs/knockout/3.1.0/knockout-min.js');
     wp_register_script('knockback', '//cdnjs.cloudflare.com/ajax/libs/knockback/0.20.5/knockback.min.js', array('backbone', 'knockout'));
