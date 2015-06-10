c6035a94eebb81fa9ceaed78c95071278ce26d07
Dave Kiss <dave@davekiss.com>
2015-06-10 10:20:31 -0400

    Add a helper function for the new lesson url

--- lib/backend/controllers/sections.php
+++ lib/backend/controllers/sections.php
@@ -7,4 +7,13 @@ class WP_Coach_Backend_Sections_Controller extends WP_Coach_Base {
   public function __construct() {
     parent::__construct();
   }
+
+  /**
+   * URL that links to the Edit Lesson iFrame
+   * @return [type] [description]
+   */
+  public function new_lesson_url() {
+    global $post;
+    return admin_url( 'admin-post.php?action=wp_coach_new_lesson&wp_coach_course_id=' . $post->ID );
+  }
 }
