87a004d1784c523ae7dbc44d092bdf30737a9692
Dave Kiss <dave@davekiss.com>
2015-06-10 10:23:13 -0400

    Include Tether Tooltip for use in admin

--- lib/backend/includes/scripts.php
+++ lib/backend/includes/scripts.php
@@ -18,10 +18,12 @@ class WP_Coach_Admin_Scripts {
     wp_register_script('magnific-popup', WP_COACH_URL . 'lib/backend/assets/bower_components/magnific-popup/dist/jquery.magnific-popup.min.js', array('jquery') );
     wp_register_style('magnific-popup', WP_COACH_URL . 'lib/backend/assets/bower_components/magnific-popup/dist/magnific-popup.css');
     wp_register_style('fontawesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css');
+    wp_register_style('tether-tooltip', WP_COACH_URL . 'lib/backend/assets/bower_components/tether-tooltip/dist/css/tooltip-theme-twipsy.css');
 
     wp_enqueue_script('magnific-popup');
     wp_enqueue_style('magnific-popup');
     wp_enqueue_style('fontawesome');
+    wp_enqueue_style('tether-tooltip');
 
     wp_register_script('sightglass', WP_COACH_URL . 'lib/backend/assets/bower_components/sightglass/index.js');
     wp_register_script('rivets', WP_COACH_URL . 'lib/backend/assets/bower_components/rivets/dist/rivets.js', array('sightglass') );
