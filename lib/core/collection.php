77a50aa83bf50e07c0589a489e785aa417e612db
Dave Kiss <dave@davekiss.com>
2015-06-10 10:16:48 -0400

    Refactor base Model and Collection implementation

--- lib/core/collection.php
+++ lib/core/collection.php
@@ -3,47 +3,76 @@
 class WP_Coach_Collection implements IteratorAggregate, JsonSerializable {
 
   private $collection = array();
-  private $collection_class;
+
+  private $owner;
+  private $target_class;
+  private $target_post_type;
+
+  private static $class_dict = array(
+    'course'   => 'WP_Coach_Course',
+    'sections' => 'WP_Coach_Section',
+    'lessons'  => 'WP_Coach_Lesson',
+  );
 
 
   /**
    * Set the collection that should be iterated
    * @param mixed $collection empty, single WP_Coach_{model}, or array of WP_Coach_{model}
    */
-  public function __construct( $collection ) {
-    
-    if ( empty( $collection) ) {
-      return;
-    }
+  public function __construct( $owner, $prop_name ) {
+    $this->owner            = $owner;
+    $this->target_class     = self::_get_class_from_prop_name( $prop_name );
+    $this->target_post_type = strtolower( $this->target_class );
 
-    if ( is_array( $collection ) ) {
-
-      $this->collection_class = get_class( $collection[0] );
-      $this->collection = $collection;
-
-    } else {
-
-      $this->collection_class = get_class( $collection );
-      $this->collection = array( $collection );
-
-    }
-
+    $this->collection = self::_get_associated($this->owner, $this->target_class, $this->target_post_type);
   }
 
 
   /**
    * Fetch the first item in a collection
-   * 
+   *
    * @return object Model
    */
   public function first() {
     return $this->collection[0];
   }
 
+
+  /**
+   * Find a model by ID in a collection
+   *
+   * @param  [type] $id [description]
+   * @return [type]     [description]
+   */
+  public function find($id = NULL) {
+    if ( ! empty( $this->collection ) ) {
+      foreach ( $this->collection as $model ) {
+        if ($model->ID == $id) {
+          return model;
+        }
+      }
+      return FALSE;
+    }
+  }
+
+
+  /**
+   * [create description]
+   * @return [type] [description]
+   */
+  public function create( $args = array() ) {
+    $target = $this->target_class;
+    $owner  = $this->owner;
+
+    $result = $target::create( $args );
+    update_post_meta($result->ID, '_' . $owner::POST_TYPE . '_id', $owner->ID );
+    return $result;
+  }
+
   /**
    * Tells the iterator which array to use
    * during iterations.
-   * 
+   *
    * @return [type] [description]
    */
   public function getIterator() {
@@ -53,11 +82,73 @@ class WP_Coach_Collection implements IteratorAggregate, JsonSerializable {
 
   /**
    * Tells the serializer to use the collection when encoding to JSON
-   * 
+   *
    * @return array
    */
   public function jsonSerialize() {
     return $this->collection;
+  }
+
+  /**
+   * [_get_class_from_prop_name description]
+   * @param  [type] $property [description]
+   * @return [type]           [description]
+   */
+  private static function _get_class_from_prop_name($property) {
+    return self::$class_dict[$property];
+  }
+
+  /**
+   * [_get_associated description]
+   * @return [type] [description]
+   */
+  private static function _get_associated($owner, $target_class, $target_post_type) {
+
+    // Run an association query with meta key
+    $query = array(
+      'posts_per_page' => -1,
+      'post_type'  => $target_post_type,
+      'perm'       => 'readable',
+      'post_status'=> 'any',
+      'meta_query' => array(
+        array(
+          'key'     => '_' . $owner::POST_TYPE . '_id',
+          'value'   => $owner->ID,
+          'compare' => '=',
+        ),
+      ),
+    );
+
+    $posts = get_posts( $query );
+
+    $collection = call_user_func_array( array( 'self', 'init_instance' ) , array($posts, $target_class) );
+    return $collection;
+  }
+
+  /**
+   * Creates a new model instance for each of the given
+   * WP_Post objects
+   *
+   * @param  mixed $posts empty, single or array of WP_Post
+   * @param  string $klass Model name
+   * @return mixed empty, single or array of Model instances
+   */
+  private static function init_instance($posts, $klass) {
+
+    if ( empty( $posts ) ) {
+      return array();
+    }
+
+    if ( is_array($posts) ) {
+      $result = array();
+      foreach( $posts as $post) {
+        $result[] = new $klass($post);
+      }
+    } else {
+      $result = new $klass($post);
+    }
+
+    return $result;
   }
 
 }
--- lib/core/model.php
+++ lib/core/model.php
@@ -3,26 +3,26 @@
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-abstract class WP_Coach_Model {
+abstract class WP_Coach_Model implements JsonSerializable {
+
+  protected $post;
 
   public function __construct( $WP_Post ) {
-    $post_vars = get_object_vars( $WP_Post );
-
-    if ( is_array( $post_vars) ) {
-      foreach ($post_vars as $key => $value) {
-        $this->{$key} = $value;
-      }
-    }
+    $this->post = get_object_vars( $WP_Post );
   }
 
 
   /**
    * Returns a collection containing models of the
    * desired post type
-   * 
+   *
+   * ToDo: Can't I just return an array here?
+   *
    * @return object
    */
   public static function all() {
+
+    return 'not implemented';
 
     $class_name = get_called_class();
     $post_type = strtolower( $class_name );
@@ -37,13 +37,13 @@ abstract class WP_Coach_Model {
     $posts = get_posts( $query );
 
     $collection = call_user_func_array( array( 'self', 'init_instance' ) , array($posts, $class_name) );
-    return new WP_Coach_Collection($collection);
+    return new WP_Coach_Collection($class_name, $collection);
   }
 
 
   /**
    * Returns a model containing the post
-   * 
+   *
    * @param  int $id Post ID
    * @return object
    */
@@ -56,27 +56,29 @@ abstract class WP_Coach_Model {
 
   /**
    * Create a new record for the calling model
-   * 
+   *
    * @return object New record
    */
-  public static function create() {
+  public static function create( $args = array() ) {
 
-    return 'not implemented';
+    $class_name = get_called_class();
+    $post_type = strtolower( $class_name );
 
-    // Validate and create lesson
-    $post_id = wp_insert_post( array(
-        'post_status'  => 'draft',
-        'post_type'    => 'wp_coach_section',
-      )
-    );
+    $post_args = array_merge( array(
+      'post_title'   => __('Untitled', 'wp-coach'),
+    ), $args );
 
-    if ( ! empty($section_id) ) {
-      update_post_meta($section_id, '_wp_coach_course_id', $this->course_id );
-      $section = get_post($section_id);
-      echo json_encode($section);
-      die;
+    // User can't change these
+    $post_args['post_type']   = $post_type;
+    $post_args['post_status'] = 'draft';
+
+    $id = wp_insert_post( $post_args );
+
+    if ( $id !== 0 ) {
+      return get_post( $id );
+    } else {
+      throw new WP_Coach_Exception( __( sprintf('Failed to insert new record for $1%s', $post_type), 'wp-coach') );
     }
-    return;
   }
 
 
@@ -109,41 +111,17 @@ abstract class WP_Coach_Model {
   public function __get( $prop_name ) {
 
     // If it is a normal or cached property, just return it
-    if ( property_exists($this, $prop_name) ) {
-      return;
+    if ( array_key_exists( $prop_name, $this->post ) ) {
+      return $this->post[$prop_name];
     }
 
     $class_name = get_class($this);
 
     if ( defined($class_name . '::HAS_MANY') && $this::HAS_MANY === $prop_name ) {
 
-      $association = 'WP_Coach_' . ucfirst( self::_depluralize( $prop_name ) );
-      $association_post_type = strtolower( $association );
-
-      // Run an association query with meta key
-      $query = array(
-        'posts_per_page' => -1,
-        'post_type'  => $association_post_type,
-        'perm'       => 'readable',
-        'meta_query' => array(
-          array(
-            'key'     => '_' . $this::POST_TYPE . '_id',
-            'value'   => $this->ID,
-            'compare' => '=',
-          ),
-        ),
-      );
-
-      if ( current_user_can('edit_wp_coach_course', $this->ID) ) {
-        $query['post_status'] = 'any';
-      }
-
-      $posts = get_posts( $query );
-      
-      $collection = call_user_func_array( array( 'self', 'init_instance' ) , array($posts, $association) );
-      $this->{$prop_name} = new WP_Coach_Collection($collection);
-
+      $this->{$prop_name} = new WP_Coach_Collection($this, $prop_name);
       return $this->{$prop_name};
+
     }
 
 
@@ -174,79 +152,20 @@ abstract class WP_Coach_Model {
     throw new Exception( __('Property does not exist on model', 'wp-coach') );
   }
 
-
   /**
-   * Creates a new model instance for each of the given
-   * WP_Post objects
+   * Include any loaded associations in json response
    * 
-   * @param  mixed $posts empty, single or array of WP_Post
-   * @param  string $klass Model name
-   * @return mixed empty, single or array of Model instances
+   * @return array
    */
-  private static function init_instance($posts, $klass) {
+  public function jsonSerialize() {
 
-    if ( empty( $posts ) ) {
-      return array();
-    }
-    
-    if ( is_array($posts) ) {
-      $result = array();
-      foreach( $posts as $post) {
-        $result[] = new $klass($post);
-      }
-    } else {
-      $result = new $klass($post);
+    $result = $this->post;
+
+    if ( defined( get_class($this) . '::HAS_MANY') && isset( $this->{$this::HAS_MANY} ) ) {
+      $result[$this::HAS_MANY] = $this->{$this::HAS_MANY};
     }
 
     return $result;
-  }
-
-
-  /**
-   * Depluralize a word
-   * 
-   * @link https://sites.google.com/site/chrelad/notes-1/pluraltosingularwithphp
-   * @param  [type] $word [description]
-   * @return [type]       [description]
-   */
-  private static function _depluralize($word){
-    // Here is the list of rules. To add a scenario,
-    // Add the plural ending as the key and the singular
-    // ending as the value for that key. This could be
-    // turned into a preg_replace and probably will be
-    // eventually, but for now, this is what it is.
-    //
-    // Note: The first rule has a value of false since
-    // we don't want to mess with words that end with
-    // double 's'. We normally wouldn't have to create
-    // rules for words we don't want to mess with, but
-    // the last rule (s) would catch double (ss) words
-    // if we didn't stop before it got to that rule.
-    $rules = array(
-        'ss' => false,
-        'os' => 'o',
-        'ies' => 'y',
-        'xes' => 'x',
-        'oes' => 'o',
-        'ies' => 'y',
-        'ves' => 'f',
-        's' => '');
-    // Loop through all the rules and do the replacement.
-    foreach(array_keys($rules) as $key){
-      // If the end of the word doesn't match the key,
-      // it's not a candidate for replacement. Move on
-      // to the next plural ending.
-      if(substr($word, (strlen($key) * -1)) != $key)
-          continue;
-      // If the value of the key is false, stop looping
-      // and return the original version of the word.
-      if($key === false)
-          return $word;
-      // We've made it this far, so we can do the
-      // replacement.
-      return substr($word, 0, strlen($word) - strlen($key)) . $rules[$key];
-    }
-    return $word;
   }
 
 }
